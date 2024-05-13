4a2036da00081535b1001fa88833c668f93f983d
Atif Ali <atif276ali@gmail.com>
2024-05-13 16:56:14 +0530

    chore: Update ProgressBox component to display dynamic message based on progress level

--- app/components/ProgressBox.tsx
+++ app/components/ProgressBox.tsx
@@ -5,13 +5,23 @@ import { ProgressBar } from "@tremor/react";
 
 export default function ProgressBox() {
   const { progress, todaysGoals, completedTodaysGoals } = useProgress();
+  let message;
+  if (progress === 0) {
+    message = "You have not started yet!";
+  } else if (progress < 50) {
+    message = "You are doing great!";
+  } else if (progress > 50 && progress < 80) {
+    message = "Almost there!";
+  } else if (progress > 80) {
+    message = "You are almost done!";
+  }
   return (
-    <section className="  flex  items-center space-x-8 bg-gradient-to-r via-blue-400 to-blue-500 from-blue-300 w-fit py-4 md:text-xl  px-7 rounded-xl ">
+    <section className="  flex max-w-md w-full  items-center space-x-8 bg-gradient-to-r via-blue-400 to-blue-500 from-blue-300 w-fit py-4 md:text-xl  px-7 rounded-xl ">
       <div>
         <Image src={ProgressTarget} alt="Progress Box" width={80} />
       </div>
       <div className="flex flex-col space-y-4">
-        <p>Your daily goal almost done</p>
+        <p>{message}</p>
         <p>
           {completedTodaysGoals} of {todaysGoals} completed
         </p>
